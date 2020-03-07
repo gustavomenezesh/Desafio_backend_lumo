85eb34d05b8645bad791df0fab3578a840ead647
gustavomenezesh <45804139+gustavomenezesh@users.noreply.github.com>
2020-03-07 00:33:33 -0300

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,11 @@
+//carrega o servidor express
 const express = require('express');
+//carrega as rotas criadas para o problema
 const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
+//Liga o servidor na porta 4444
 app.listen(4444);
