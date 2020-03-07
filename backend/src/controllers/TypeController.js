212a4d301db27ff79412df151db6006b4c6bd728
gustavomenezesh <45804139+gustavomenezesh@users.noreply.github.com>
2020-03-07 00:30:20 -0300

    Update TypeController.js

--- backend/src/controllers/TypeController.js
+++ backend/src/controllers/TypeController.js
@@ -1,16 +1,19 @@
-
+//Ler os dados no arquivo .JSON
 const dados = require('../dados.json')
 
 module.exports = {
 
     async index(request, response){
         
+        //Captura o tipo de descrição requisitado pelo cliente
         const tipo = request.body;
         
         var aux = [];
+        
+        //Percorre todas as proposições
         for(var i = 0; i < dados.dados.length; i++){
-            if(dados.dados[i].descricaoTipo == tipo.tipo){
-                aux.push(dados.dados[i]);
+            if(dados.dados[i].descricaoTipo == tipo.tipo){              //Só coloca no vetor de proposições que irá ser retornado se
+                aux.push(dados.dados[i]);                               //o tipo da proposição for o tipo solicitado pelo cliente
             }
         }
 
