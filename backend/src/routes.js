9101c50e6b1dec7517e28c8a837c93faadbdf66b
gustavomenezesh <45804139+gustavomenezesh@users.noreply.github.com>
2020-03-07 00:41:04 -0300

    Update routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
+//Carrega o objeto Router
 const {Router} = require('express')
+
+//carrega o tratamento criado para as rotas
 const ListController = require('./controllers/ListController')
 const TypeController = require('./controllers/TypeController')
 const AutorController = require('./controllers/AutorController')
 
-
+//Instancia o objeto um objeto Router
 const routes = Router()
 
+//Linkando as rotas
 routes.get('/list', ListController.index)
 routes.get('/typelist', TypeController.index)
 routes.get('/authorlist', AutorController.index)
